ecd01e8b11ec547468ffc37e1a35033a3fe8ab93
Ajay Paliwal <ajaypaliwal.cto@outlook.com>
2019-03-30 11:49:17 +0530

    test
    test

---
+++ precache-manifest.813cab6c06ca9740015bc796863d0988.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f17721ebb29a0ac2fecf",
+    "url": "/static/css/main.9d90a94f.chunk.css"
+  },
+  {
+    "revision": "f17721ebb29a0ac2fecf",
+    "url": "/static/js/main.984cf77b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "674aeee80a619c4cc528a9c045785660",
+    "url": "/index.html"
+  }
+];
